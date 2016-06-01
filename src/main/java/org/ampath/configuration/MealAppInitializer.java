a4067853b1645aa6fe70e2773782d40b24621235
Vincent Tuwei <vincent.tuwei@movasgroup.com>
2016-06-01 16:07:05 +0300

    setup spring DI

---
+++ src/main/java/org/ampath/configuration/MealAppInitializer.java
@@ -0,0 +1,30 @@
+package org.ampath.configuration;
+
+import javax.servlet.Filter;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class MealAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+ 
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[] { MealAppConfiguration.class };
+    }
+  
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return null;
+    }
+  
+    @Override
+    protected String[] getServletMappings() {
+        return new String[] { "/" };
+    }
+    
+    @Override
+    protected Filter[] getServletFilters() {
+    	Filter [] singleton = { new CORSFilter() };
+    	return singleton;
+	}
+ 
+}
